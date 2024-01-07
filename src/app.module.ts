3e46d05fbedf2314b82522ea72df881af342a82d
Juan Pablo Victoria <44125388+juanp288@users.noreply.github.com>
2024-01-06 19:47:35 -0500

    feat: Added ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { MailModule } from './mail/mail.module';
-import { CommonModule } from './common/common.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [MailModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MailModule,
+  ],
 })
 export class AppModule {}
